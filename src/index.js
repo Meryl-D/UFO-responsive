228c7f2759d4a487db2ea93be589a4a0786d5be7
Meryl Dubois <meryl.dubois@heig-vd.ch>
2022-03-23 16:56:24 +0100

    fonction de tri par récurrence des formes d'OVNIs ajoutée

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import * as d3 from 'd3';
 import ufoData from '../data/ufo-sighting/complete.csv';
-console.log(ufoData);
+
+const noEmptyData = ufoData.filter(d => d.shape !== null && d.comments !== null && d.country === 'us')
+
+const orderByOccurrence = (data, property) => {
+    let counts = {};
+
+    data.forEach(value => {
+        if(!counts[value[property]]) counts[value[property]] = 0;
+        counts[value[property]]++;
+    });
+    console.log(counts);
+    // return counts;
 
 
+    return Object.entries(counts).sort((a,b) => b[1]-a[1]);
+}
 
+const shapeFrequency = orderByOccurrence(noEmptyData, 'shape');
+console.log(shapeFrequency);
