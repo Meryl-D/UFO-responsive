64ee6f306b110d342efb90d520eff1777e376f2f
Meryl Dubois <meryl.dubois@heig-vd.ch>
2022-05-08 19:37:28 +0200

    hid body during loading

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import '/src/css/index.css';
-import '/src/js/pageTwo/pageTwo.js';
+import { renderMap } from '/src/js/pageTwo/map.js';
+import { storage } from '/src/js/lib/storage.js';
+import { renderChart, renderMovie } from '/src/js/pageTwo/card.js';
+
+if (window.location.pathname != '/') {
+
+    storage.setItem('shape', 'circle');
+
+    renderMap();
+    renderChart();
+    renderMovie();
+
+}
--- src/js/pageTwo/pageTwo.js
+++
@@ -1,9 +0,0 @@
-import { renderMap } from './map.js';
-import { storage } from '../lib/storage.js';
-import { renderChart, renderMovie } from './card.js';
-
-storage.setItem('shape', 'triangle');
-
-renderMap();
-renderChart();
-renderMovie();
