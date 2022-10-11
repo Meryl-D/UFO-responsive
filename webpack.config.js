0900f9397cb0c7f25cab95401b7980364539254e
Meryl Dubois <meryl.dubois@heig-vd.ch>
2022-10-11 13:50:06 +0200

    add module option for css

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,10 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader", option: { modules: true } }
+                ],
             },
             { 
                 test: /\.js$/,
