6176ecf260915bb6150cadacaac591edcaf318a2
Meryl Dubois <meryl.dubois@heig-vd.ch>
2022-10-24 13:01:39 +0200

    change webpack config for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
     name: 'browser',
-    mode: 'development',
+    mode: 'production',
+    devtool: 'source-map',
     entry: {
         index: './src/index.js',
         mapModule: './src/mapModule.js',
@@ -18,23 +21,50 @@ module.exports = {
         runtimeChunk: 'single',
         splitChunks: {
           cacheGroups: {
-            vendor: {
-              test: /[\\/]node_modules[\\/]/,
-              name: 'vendors',
-              chunks: 'all'
+            // vendor: {
+            //   test: /[\\/]node_modules[\\/]/,
+            //   name: 'vendors',
+            //   chunks: 'all'
+            // },
+            styles: {
+                name: "styles",
+                type: "css/mini-extract",
+                chunks: "all",
+                enforce: true,
             }
           }
-        }
+        },
+        minimizer: [
+            new CssMinimizerPlugin(),
+          ],
     },
     performance: {
-        maxAssetSize: 800000,
-        maxEntrypointSize: 800000,
+        maxAssetSize: 1100000,
+        maxEntrypointSize: 1100000,
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html',
+            inject: 'body',
+        }),
+
+        new HtmlWebpackPlugin({
+            template: './public/map.html',
+            filename: 'map.html'
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css",
+            ignoreOrder: false, // Enable to remove warnings about conflicting order
+        })
+    ],
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
             },
             { 
                 test: /\.js$/,
@@ -56,18 +86,5 @@ module.exports = {
                 type: 'asset/resource',
             }
         ]
-    },
-
-    plugins: [
-    new HtmlWebpackPlugin({
-        template: './public/index.html',
-        filename: 'index.html',
-        inject: 'body',
-    }),
-
-    new HtmlWebpackPlugin({
-        template: './public/map.html',
-        filename: 'map.html'
-      })
-    ]
+    }
 }
